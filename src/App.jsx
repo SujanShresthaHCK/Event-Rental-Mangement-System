3fde95f5d6ba2d81e0f7d43ead9724b158c54c4a
Sujan Shrestha <118831645+SujanShresthaHCK@users.noreply.github.com>
2024-04-02 12:27:25 +0545

    node implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./assets/pages/Home";
 import BookVenue from "./assets/pages/BookVenue";
 
 function App() {
+  const [backendData, setBackendData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
   return (
     <>
       <BrowserRouter>
